95e518a2558191bcd2bf317ea371720777d99256
nafsol <talhabinafzal@outlook.com>
2021-08-19 13:15:11 +0500

    First commit

---
+++ Controllers/TableController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SbAdmin.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Tables()
+        {
+            return View();
+        }
+    }
+}
